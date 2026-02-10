73418c47273fcd0287e8edfb907b77939c76fca4
Tetsuo Kitamura <kitamura.tetsuo@gmail.com>
2026-02-10 20:57:22 +0900

    Delete battle visualization HTML file, add it to gitignore, and modify the battle script.

--- scripts/battle.sh
+++ scripts/battle.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PYTHONPATH=$(pwd)
 uv run src/battle.py \
-    --checkpoint "checkpoints_sample/checkpoint_transformer.pkl" \
+    --checkpoint "checkpoints/checkpoint_100.pkl" \
     --deck_id_1 "deckgym-core/example_decks/mewtwoex.txt" \
     --deck_id_2 "deckgym-core/example_decks/blastoiseex.txt" \
     --device "cpu"
